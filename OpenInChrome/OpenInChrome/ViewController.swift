ce728e338e035a229bef99ffdd4aa55097636817
Elena <o.shalena@gmail.com>
2017-01-13 18:59:19 +0200

    commit with central indicator and view==lost internet connection

--- OpenInChrome/OpenInChrome/ViewController.swift
+++ OpenInChrome/OpenInChrome/ViewController.swift
@@ -1,18 +1,17 @@
 
 
 import UIKit
-
+import SystemConfiguration
 class ViewController: UIViewController, UIScrollViewDelegate, UIWebViewDelegate {
     
     @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var popup: UIView!
     @IBOutlet weak var indicator: UIActivityIndicatorView!
     var statusBarStyle: UIStatusBarStyle = .lightContent
 
     override func viewDidLoad() {
         super.viewDidLoad()
-      // var refreshControl = UIRefreshControl()
 
-//        self.webView.scrollView.delegate = self;
         webView.scrollView.delegate = self
         webView.delegate = self;
         self.navigationController?.setNavigationBarHidden(true, animated: false)
@@ -21,7 +20,6 @@ class ViewController: UIViewController, UIScrollViewDelegate, UIWebViewDelegate 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         detectChromeInstallation();
-
     }
     
     
@@ -30,50 +28,80 @@ class ViewController: UIViewController, UIScrollViewDelegate, UIWebViewDelegate 
     }
  
     func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-        if (scrollView.contentOffset.y < -70){
+        if (scrollView.contentOffset.y < -70) {
             //reach top
             print("Reach Top")
-            webView.reload()
-            indicator.startAnimating()}
-        
-      
+            
+            if (self.webView.request != nil) {
+                webView.reload()
+            } else {
+                let inputURL = URL(string: "https://demo-tecsynt.atiim.com")!
+                self.webView.loadRequest(URLRequest.init(url: inputURL))
+            }
+            
+            let flag = self.isInternetAvailable()
+            if flag {
+                if !self.popup.isHidden {
+                    self.popup.isHidden = true
+                }
+                indicator.startAnimating()
+            } else {
+                webView.stringByEvaluatingJavaScript(from: "document.open();document.close()")
+                if self.popup.isHidden {
+                    self.popup.isHidden = false
+                }
+            }
+            }
     }
+
     
     func webViewDidFinishLoad(_ webView: UIWebView) {
         indicator.stopAnimating();
+        if !self.popup.isHidden {
+            self.popup.isHidden = true
+        }
     }
     
     func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
         indicator.stopAnimating();
-    }
-    // MARK: - UIViewController
-    
-    
-    
-//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-//        if (scrollView.contentOffset.x > 0) {
-//            scrollView.contentOffset = CGPoint.init(x: 0, y: scrollView.contentOffset.y)
-//        }
-//    }
-//    
-    func detectChromeInstallation() {
-        let inputURL = URL(string: "https://demo-tecsynt.atiim.com")!
+        if self.popup.isHidden {
+            self.popup.isHidden = false
+        }
         
-//        if OpenInChromeController.sharedInstance.isChromeInstalled() {
-//            let callbackURL = URL(string: "opcsample://")
-//            if !OpenInChromeController.sharedInstance.openInChrome(inputURL, callbackURL: callbackURL, createNewTab: true) {
-//                log("[Failed] Cannot open \(inputURL.absoluteString) in Google Chrome. Will try to open it with system openURL")
-//                if (UIApplication.shared.openURL(inputURL)) {
-//                    log("[Success] Opened \(inputURL.absoluteString) by system openURL.")
-//                } else {
-//                    log("[Failed] Cannot open \(inputURL.absoluteString) by system openURL.")
-//                }
-//            } else {
-//                log("[Success] Opened \(inputURL.absoluteString) in Google Chrome.")
-//            }
-//        } else {
+    }
+    
+    func isInternetAvailable() -> Bool
+    {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+        
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        return (isReachable && !needsConnection)
+    }
+    
+    func detectChromeInstallation() {
+        let flag = self.isInternetAvailable()
+        if flag {
+            let inputURL = URL(string: "https://demo-tecsynt.atiim.com")!
             self.webView.loadRequest(URLRequest.init(url: inputURL))
             indicator.startAnimating();
+        } else {
+            if self.popup.isHidden {
+                self.popup.isHidden = false
+            }
+        }
         }
     }
 
